3c02db41b3ecf2897e1e9e7adad46b42e01fc707
Steve Pentler <stevepentler@gmail.com>
2015-11-17 23:17:52 -0700

    holy fuck. jordan's wizardry worked. statewide analysis refactored and passing before across tests

--- lib/statewide_testing_analysis.rb
+++ lib/statewide_testing_analysis.rb
@@ -9,29 +9,48 @@ class StatewideTestingAnalysis
 
   def top_statewide_test_year_over_year_growth(testing_categories)
     input_error?(testing_categories)
-    if testing_categories.has_key?(:subject)
-      top = nil
-      if testing_categories.has_key?(:top)
-        top = testing_categories[:top]
-      end
-      (district_subject_growths(testing_categories).max(top) {|pair| pair[1]})
-    else
-      change = @statewide_tests.map do |state_test|
-        if state_test.name != "COLORADO"
-         district_growths_across_subjects(testing_categories, state_test)
-        end
-      end
-    end 
+    all_district_growths = district_subject_growths(testing_categories)
+    top_districts(testing_categories, all_district_growths)
+  end
+  #   if testing_categories.has_key?(:subject)
+  #     top = nil
+  #     if testing_categories.has_key?(:top)
+  #       top = testing_categories[:top]
+  #     end
+  #     (district_subject_growths(testing_categories).max(top) {|pair| pair[1]})
+  #   else
+  #     change = @statewide_tests.map do |state_test|
+  #       if state_test.name != "COLORADO"
+  #        district_growths_across_subjects(testing_categories, state_test)
+  #       end
+  #     end
+  #   end 
+  # end 
+
+  def top_districts(testing_categories, all_district_growths)
+    top = nil
+    if testing_categories.has_key?(:top)
+      top = testing_categories[:top]
+    end
+    (all_district_growths.max(top) {|pair| pair[1]})
   end 
 
   def district_subject_growths(testing_categories)
     change = @statewide_tests.map do |state_test|
       if state_test.name != "COLORADO"
-        [state_test.name, (find_growth_for_subjects(testing_categories, state_test))]
+        [state_test.name, route_by_subject(testing_categories, state_test)]
       end
     end 
     change.compact
   end
+
+  def route_by_subject(testing_categories, state_test)
+    if testing_categories.has_key?(:subject)
+      find_growth_for_subjects(testing_categories, state_test)
+    else
+      district_growths_across_subjects(testing_categories, state_test)
+    end 
+  end 
 
   def district_growths_across_subjects(testing_categories, state_test)
     subjects = [:math, :writing, :reading]
@@ -39,7 +58,8 @@ class StatewideTestingAnalysis
       testing_categories[:subject] = single_subject
       {single_subject => (find_growth_for_subjects(testing_categories, state_test))}
     end
-    [state_test.name, values.compact]
+    values
+    # (values.compact.inject(:+) / 3)
   end
 
   def find_growth_for_subjects(testing_categories, state_test)
@@ -56,91 +76,6 @@ class StatewideTestingAnalysis
   def truncate(float)
     (float * 1000).floor / 1000.to_f
   end
-
-  # def subject_leader(testing_categories)
-    
-  #   @district_repo.statewide_testing_repo.statewide_tests.map do |test|
-  #     test
-  #   end 
-
-
-
-  #   #find the first year for each district in subject and take data value
-  #   # => final year - first year / 2 
-  #     #map district name and value into array so that if can be sorted by value
-
-
-  # end 
-
-
-
-
-
-  #   elsif ###find leading district for grade
-  #     # growth_ranking = []
-  #     # @district_repo.statewide_testing.find_by_name(district).each do |district|
-  #     #   growth_ranking << [district, yearly_change(district)]
-  #     # end 
-  #     # growth_ranking.sort_by! {|district, growth| growth}
-  #     # growth_ranking.last
-    
-
-
-  #   elsif testing_categories.has_key?(:top)
-  #     growth_ranking.last(testing_categories[:top]) 
-
-    
-
-  #   elsif testing_categories.has_key?(:weighting)
-  #     @district_repo.statewide_testing.find_by_name(district).each do |district|
-  #       weighted_growth_ranking << [district, yearly_change(district)]
-  #     end 
-  #   end
-
-  # end 
-
-
-  # def subject_leader(testing_categories)
-  #   scores_array = @district_repo.statewide_testing.----------map do |district|
-  #     [district.name, yearly_growth(testing_categories, name)]
-  #   scores_array.sort_by! {|district, growth| growth}
-  #   scores_array.last
-  #   end 
-  # end 
-
-  # def final_year_data(input_array)
-  # end 
-
-  # def first_year_data(input_array)
-  # end
-
-  # def growth_calculation
-  # end 
-
-  # def yearly_growth(input_array)
-  #   subtractions = []
-  #   if input_array.length >= 2 
-  #     subtractions << (input_array[1] - input_array[0])
-  #     input_array.shift
-  #   else 
-  #     subtractions.inject(:+) / subtractions.length
-  #   end 
-  # end 
-
-
-  # def determine_weighting(testing_categories)
-  #   unless testing_categories.has_key?(:weighting)
-  #     testing_categories[:weighting] = even_weighting
-  #   end 
-  # end
-
-  # def combined_weights_equals_one?(testing_categories)
-  #   unless testing_categories[:weighting].values.inject(:+) == 1.0
-  #     raise InsufficientInformationError, 
-  #       "Weighted sum does not equal one"
-  #   end  
-  # end  
-
  
   def input_error?(testing_categories)
     unless testing_categories.keys.include?(:grade)
